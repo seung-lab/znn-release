08dc165cae289432f91064eb5aaf79b6569f445b
torms3 <kisuklee@mit.edu>
2015-01-08 18:59:15 -0500

    global rebalancing as a default

--- src/core/network.hpp
+++ src/core/network.hpp
@@ -622,40 +622,40 @@ public:
         std::list<double3d_ptr> grads = cost_fn_->gradient(outs,s->labels,s->masks);
         
         // rebalancing (global)
-        // if ( op->rebalance )
-        // {
-        //     std::list<double3d_ptr>::iterator wit = s->wmasks.begin();
-        //     FOR_EACH( it, grads )
-        //     {
-        //         volume_utils::elementwise_mul_by(*it, *wit++);
-        //     }
-        // }
-        // rebalancing (patch-wise)
         if ( op->rebalance )
         {
-            // default cross-entropy is multinomial (1-of-K coding)
-            if ( op->cost_fn == "cross_entropy" )
+            std::list<double3d_ptr>::iterator wit = s->wmasks.begin();
+            FOR_EACH( it, grads )
             {
-                double3d_ptr rbmask = 
-                    volume_utils::multinomial_rebalance_mask(s->labels);
-
-                FOR_EACH( it, grads )
-                {
-                    volume_utils::elementwise_mul_by(*it,rbmask);
-                }
-            }
-            else
-            {
-                std::list<double3d_ptr> rbmask = 
-                    volume_utils::binomial_rebalance_mask(s->labels);
-
-                std::list<double3d_ptr>::iterator rbmit = rbmask.begin();
-                FOR_EACH( it, grads )
-                {
-                    volume_utils::elementwise_mul_by(*it,*rbmit++);
-                }
+                volume_utils::elementwise_mul_by(*it, *wit++);
             }
         }
+        // rebalancing (patch-wise)
+        // if ( op->rebalance )
+        // {
+        //     // default cross-entropy is multinomial (1-of-K coding)
+        //     if ( op->cost_fn == "cross_entropy" )
+        //     {
+        //         double3d_ptr rbmask = 
+        //             volume_utils::multinomial_rebalance_mask(s->labels);
+
+        //         FOR_EACH( it, grads )
+        //         {
+        //             volume_utils::elementwise_mul_by(*it,rbmask);
+        //         }
+        //     }
+        //     else
+        //     {
+        //         std::list<double3d_ptr> rbmask = 
+        //             volume_utils::binomial_rebalance_mask(s->labels);
+
+        //         std::list<double3d_ptr>::iterator rbmit = rbmask.begin();
+        //         FOR_EACH( it, grads )
+        //         {
+        //             volume_utils::elementwise_mul_by(*it,*rbmit++);
+        //         }
+        //     }
+        // }
 
         if ( op->norm_grad )
         {
