3d3b47a5e90c5712316525424e53a0ff37e57027
Aleks <zlateski@mit.edu>
2015-03-28 19:15:36 -0700

    parallel edge working

--- src/core/v4/network/parallel/edge.hpp
+++ src/core/v4/network/parallel/edge.hpp
@@ -3,12 +3,30 @@
 #include "../../assert.hpp"
 #include "../../types.hpp"
 #include "../../cube/cube.hpp"
+#include "../../utils/task_manager.hpp"
+
 
 namespace znn { namespace v4 { namespace parallel_network {
 
+class nodes;
+class edges;
+
 class edge
 {
+protected:
+    nodes * in_nodes ;
+    size_t  in_num   ;
+    nodes * out_nodes;
+    size_t  out_num  ;
+
+    task_manager & manager;
+
 public:
+    edge( nodes * in, size_t inn, nodes * out, size_t outn, tasks_manager & m )
+        : in_nodes(in), in_num(inn), out_nodes(out), out_num(outn), manager(m)
+    {
+    }
+
     virtual ~edge() {}
 
     virtual void forward( ccube_p<double> const & )
@@ -22,6 +40,10 @@ public:
 
     virtual void backward( ccube_p<complex> const & )
     { UNIMPLEMENTED(); }
+
+    virtual void zap(edges*) = 0;
 };
+
+
 
 }}} // namespace znn::v4::parallel_network
