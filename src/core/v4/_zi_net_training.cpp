9ac6e04a90478acd513e4f6cf23387aa34ec1b01
Aleksandar Zlateski <zlateski@mit.edu>
2015-05-19 20:38:32 -0400

    zi_net test added

--- src/core/v4/_zi_net_training.cpp
+++ src/core/v4/_zi_net_training.cpp
@@ -5,8 +5,8 @@ using namespace znn::v4;
 
 int main(int argc, char** argv)
 {
-    std::vector<options> nodes(7);
-    std::vector<options> edges(6);
+    std::vector<options> nodes(8);
+    std::vector<options> edges(7);
 
     nodes[0].push("name", "input")
         .push("type", "input")
@@ -25,7 +25,6 @@ int main(int argc, char** argv)
         .push("function", "rectify_linear")
         .push("size", 16);
 
-
     edges[1].push("name", "conv2")
         .push("type", "conv")
         .push("init", "uniform")
@@ -37,7 +36,7 @@ int main(int argc, char** argv)
     nodes[2].push("name", "nl2")
         .push("type", "transfer")
         .push("function", "rectify_linear")
-        .push("size", 10);
+        .push("size", 24);
 
     edges[2].push("name", "conv3")
         .push("type", "conv")
@@ -50,7 +49,7 @@ int main(int argc, char** argv)
     nodes[3].push("name", "nl3")
         .push("type", "transfer")
         .push("function", "rectify_linear")
-        .push("size", 10);
+        .push("size", 32);
 
     edges[3].push("name", "conv4")
         .push("type", "conv")
@@ -63,7 +62,7 @@ int main(int argc, char** argv)
     nodes[4].push("name", "nl4")
         .push("type", "transfer")
         .push("function", "rectify_linear")
-        .push("size", 10);
+        .push("size", 32);
 
     edges[4].push("name", "conv5")
         .push("type", "conv")
@@ -76,12 +75,12 @@ int main(int argc, char** argv)
     nodes[5].push("name", "nl5")
         .push("type", "transfer")
         .push("function", "rectify_linear")
-        .push("size", 10);
+        .push("size", 32);
 
     edges[5].push("name", "conv6")
         .push("type", "conv")
         .push("init", "uniform")
-        .push("size", "1,1,1")
+        .push("size", "7,7,7")
         .push("stride", "1,1,1")
         .push("input", "nl5")
         .push("output", "nl6");
@@ -89,7 +88,21 @@ int main(int argc, char** argv)
     nodes[6].push("name", "nl6")
         .push("type", "transfer")
         .push("function", "rectify_linear")
+        .push("size", 32);
+
+    edges[6].push("name", "conv7")
+        .push("type", "conv")
+        .push("init", "uniform")
+        .push("size", "1,1,1")
+        .push("stride", "1,1,1")
+        .push("input", "nl6")
+        .push("output", "nl7");
+
+    nodes[7].push("name", "nl7")
+        .push("type", "transfer")
+        .push("function", "rectify_linear")
         .push("size", 1);
+
 
 
     int64_t x = 9;
