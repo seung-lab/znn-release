9c47f112b6aa364d7f940dd1a7fb9538344b7cb4
jingpengwu <jingpeng.wu@gmail.com>
2015-07-27 05:34:27 -0400

    should revise output size

--- python/pyznn.cpp
+++ python/pyznn.cpp
@@ -27,6 +27,7 @@
 #include <string>
 #include <memory>
 #include <cstdint>
+#include <assert.h>
 // znn
 #include "network/parallel/network.hpp"
 #include "cube/cube.hpp"
@@ -57,25 +58,19 @@ np::ndarray CNet_forward( bp::object const & self, const np::ndarray& inarray )
 	// extract the class from self
 	network& net = boost::python::extract<network&>(self)();
 
-	// volume size
+	// input volume size
 	std::size_t sz = inarray.shape(0);
 	std::size_t sy = inarray.shape(1);
 	std::size_t sx = inarray.shape(2);
-    vec3i insize(sz,sy,sx);
     
-	// setup input volume
-    // std::cout<<"create incube..."<<std::endl;
-	// boost::multi_array_ref<real,3> incube_ref( reinterpret_cast<real*>(inarray.get_data()), extents[sz][sy][sx] );
-	// cube<real> incube( incube_ref );
-
-    cube_p<real> incube_p = get_cube<real>(insize);
+    // copy data to avoid the pointer free error
+    cube_p<real> incube_p = get_cube<real>(vec3i(sz,sy,sx));
     for (std::size_t k=0; k<sz*sy*sx; k++)
         incube_p->data()[k] = inarray.get_data()[k];
 
     std::cout<<"put incube to insample..."<<std::endl;
 	std::map<std::string, std::vector<cube_p< real >>> insample;
 	insample["input"].resize(1);
-    //insample["input"][0] = std::shared_ptr<cube<real> >( &incube );
     insample["input"][0] = incube_p;
 
     // run forward and get output
@@ -83,20 +78,16 @@ np::ndarray CNet_forward( bp::object const & self, const np::ndarray& inarray )
     auto prop = net.forward( std::move(insample) );
     cube<real> out_cube(*prop["output"][0]);
     
-    // copy data to a new volume to let python free the out_cube
-    //std::size_t n = static_cast<std::size_t>( out_cube.num_elements() );
-    //real out[n];
-    //for (std::size_t i=0; i<n; i++)
-        //out[i] = out_cube.data()[i];
-    //cube_p<real> out_p = get_copy( out_cube );
-
+    // output size
+    //
+    //
     // create a PyObject * from pointer and data to return
     std::cout<<"build return ndarray..."<<std::endl;
     np::ndarray ret = np::from_data(
 		out_cube.data(),
 		np::dtype::get_builtin<real>(),
-		bp::make_tuple(sx,sy,sz),
-		bp::make_tuple(sy*sx*sizeof(real), sx*sizeof(real), sizeof(real)),
+		bp::make_tuple(osx,osy,osz),
+		bp::make_tuple(osy*osx*sizeof(real), osx*sizeof(real), sizeof(real)),
 		self
 	);
     std::cout<<"return the output array..."<<std::endl;
