f4281ce7712de7b5be332872347bbd78750345a8
Aleksandar Zlateski <zlateski@mit.edu>
2015-07-29 18:09:54 -0400

    scripts

---
+++ src/cpp/measurements/make_gcc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -std=c++11 $1.cpp -I../../.. -I../../../src/include -DNDEBUG -O3 -DZNN_CUBE_POOL_LOCKFREE -DZNN_USE_FLOATS -lpthread -lrt -lfftw3f -ljemalloc -o $1 -DZNN_DONT_CACHE_FFTS
