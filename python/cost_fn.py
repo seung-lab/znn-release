fix a bug of softmax

python/cost_fn.py
@@ -60,8 +60,9 @@ def softmax(props):
     Returns:
     ret:   numpy array, softmax activation volumes
     """
-    pesum = np.sum(np.exp(props), axis=0)
-    ret = props / pesum
+    props_exp = np.exp(props)
+    pesum = np.sum(props_exp, axis=0)
+    ret = props_exp / pesum
     return ret
 
 #@jit(nopython=True)

python/train.py
@@ -40,7 +40,7 @@ terr_list = list()
 tcls_list = list()
 it_list = list()
 # the temporal weights
-rb_weights=[] 
+rb_weights=[]
 malis_weights=[]
 grdts_bm=[]
 
@@ -53,20 +53,20 @@ for i in xrange(1, tpars['Max_iter'] ):
     vol_in, lbl_out = front_end.get_sample( vol_orgs, insz, lbl_orgs, outsz )
     if tpars['is_data_aug']:
         vol_in, lbl_out = front_end.data_aug( vol_in, lbl_out )
-        
+
     # forward pass
     prop = net.forward( np.ascontiguousarray(vol_in) ).astype('float32')
 
     # softmax
     if tpars['cost_fn_str']=='multinomial_cross_entropy':
         prop = cost_fn.softmax(prop)
-    
+
     # cost function and accumulate errors
     cerr, grdt = tpars['cost_fn']( prop.astype('float32'), lbl_out.astype('float32') )
     ccls = np.count_nonzero( (prop>0.5)!= lbl_out )
     err = err + cerr
     cls = cls + ccls
-    
+
     # rebalance
     if tpars['is_rebalance']:
         rb_weight = cost_fn.rebalance( lbl_out )
@@ -74,11 +74,11 @@ for i in xrange(1, tpars['Max_iter'] ):
     if tpars['is_malis']:
         grdt_bm = np.copy(grdt)
         malis_weight = cost_fn.malis_weight(prop)
-        grdt = grdt * malis_weight 
-    
+        grdt = grdt * malis_weight
+
     # run backward pass
     net.backward( np.ascontiguousarray(grdt) )
-    
+
     if i%tpars['Num_iter_per_show']==0:
         # anneal factor
         eta = eta * tpars['anneal_factor']
@@ -86,7 +86,7 @@ for i in xrange(1, tpars['Max_iter'] ):
         # normalize
         err = err / float(tpars['Num_iter_per_show'] * prop.shape[0] * outsz[0] * outsz[1] * outsz[2])
         cls = cls / float(tpars['Num_iter_per_show'] * prop.shape[0] * outsz[0] * outsz[1] * outsz[2])
-        
+
         err_list.append( err )
         cls_list.append( cls )
         it_list.append( i )
@@ -99,7 +99,7 @@ for i in xrange(1, tpars['Max_iter'] ):
                                         eta*float(outsz[0] * outsz[1] * outsz[2]), \
                                         vol_in, prop, lbl_out, grdt, tpars, \
                                         rb_weights, malis_weights, grdt_bm)
-        
+
         # save network
         print "save network"
         front_end.save_network(net, gpars['fnet'])