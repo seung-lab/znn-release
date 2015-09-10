b51686136ff5856a2c8445a2c5835add0510c575
Kisuk Lee <kisuklee@mit.edu>
2015-09-10 10:18:08 -0400

    backward should be called before saving net

--- python/train.py
+++ python/train.py
@@ -19,12 +19,12 @@ def main( conf_file='config.cfg' ):
     config, pars = front_end.parser( conf_file )
     smp_trn = front_end.CSamples(pars['train_range'], config, pars)
     smp_tst = front_end.CSamples(pars['test_range'],  config, pars)
-    
+
     #%% create and initialize the network
-    
+
     outsz = pars['train_outsz']
     print "output volume size: {}x{}x{}".format(outsz[0], outsz[1], outsz[2])
-    
+
     if pars['train_load_net']:
         print "loading network..."
         net = netio.load_network( pars['train_load_net'], pars['fnet_spec'], outsz, pars['num_threads'])
@@ -37,67 +37,67 @@ def main( conf_file='config.cfg' ):
 
     # number for normalization
     tn = pars['Num_iter_per_show'] * outsz[0] * outsz[1] * outsz[2]
-    
+
     #%% compute inputsize and get input
     fov = np.asarray(net.get_fov())
     print "field of view: {}x{}x{}".format(fov[0],fov[1], fov[2])
     insz = fov + outsz - 1
-    
+
     # initialization
     err = 0;
     cls = 0;
     err_list = list()
     cls_list = list()
     it_list = list()
-    
+
     terr_list = list()
     tcls_list = list()
     titr_list = list()
     # the temporal weights
     malis_weights=[]
-    
+
     # interactive visualization
     plt.ion()
     plt.show()
-    
+
     start = time.time()
     for i in xrange(1, pars['Max_iter'] ):
         vol_ins, lbl_outs = smp_trn.get_random_sample( insz, outsz )
-    
+
         # forward pass
         props = net.forward( utils.loa_as_continue(vol_ins, dtype='float32') )
-    
+
         # cost function and accumulate errors
         props, cerr, grdts = pars['cost_fn']( props, lbl_outs )
         err = err + cerr
         cls = cls + cost_fn.get_cls(props, lbl_outs)
-    
+
         if pars['is_malis'] :
             malis_weights = cost_fn.malis_weight(props, lbl_outs)
             grdts = utils.loa_mul(grdts, malis_weights)
-    
-        
-        
+
+        # run backward pass
+        net.backward( utils.loa_as_continue(grdts, dtype='float32') )
+
         if i%pars['Num_iter_per_test']==0:
             # test the net
             terr_list, tcls_list = test.znn_test(net, pars, smp_tst,\
                                     insz, outsz, terr_list, tcls_list)
             titr_list.append(i)
-    
+
         if i%pars['Num_iter_per_show']==0:
             # anneal factor
             eta = eta * pars['anneal_factor']
             net.set_eta(eta)
             # normalize
-            
+
             err = err / tn
             cls = cls / tn
-    
+
             err_list.append( err )
             cls_list.append( cls )
             it_list.append( i )
-            
-                
+
             # show results To-do: run in a separate thread
             front_end.inter_show(start, i, err, cls, it_list, err_list, cls_list, \
                                     titr_list, terr_list, tcls_list, \
@@ -112,17 +112,14 @@ def main( conf_file='config.cfg' ):
             cls = 0
             # reset time
             start = time.time()
-            
+
         if i%pars['Num_iter_per_save']==0:
             # save network
             print "save network"
             netio.save_network(net, pars['train_save_net'], num_iters=i)
             utils.save_statistics( pars, it_list, err_list, cls_list,\
                                     titr_list, terr_list, tcls_list)
-        
-        # run backward pass
-        net.backward( utils.loa_as_continue(grdts, dtype='float32') )
-        
+
 if __name__ == '__main__':
     import sys
     if len(sys.argv)>1:
