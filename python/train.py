ead4e400513d0aee565e0cddd6f83787000532c3
jingpengwu <jingpeng.wu@gmail.com>
2015-09-10 21:33:32 -0400

    add configuration checking

--- python/train.py
+++ python/train.py
@@ -17,11 +17,11 @@ def main( conf_file='config.cfg' ):
     #%% parameters
     print "reading config parameters..."
     config, pars = front_end.parser( conf_file )
-    
+
     #%% create and initialize the network
     outsz = pars['train_outsz']
     print "output volume size: {}x{}x{}".format(outsz[0], outsz[1], outsz[2])
-    
+
     if pars['train_load_net']:
         print "loading network..."
         net = netio.load_network( pars['train_load_net'], pars['fnet_spec'], outsz, pars['num_threads'])
@@ -34,15 +34,18 @@ def main( conf_file='config.cfg' ):
     eta = pars['eta'] / vn
     net.set_eta( eta )
     net.set_momentum( pars['momentum'] )
-    
+
     # get input and output information
     info_in  = net.get_inputs()
     info_out = net.get_outputs()
-    
+
     # initialize samples
     print "create input output samples..."
     smp_trn = front_end.CSamples(config, pars, pars['train_range'], info_in, info_out)
     smp_tst = front_end.CSamples(config, pars, pars['test_range'],  info_in, info_out)
+
+    # check all the settings
+    utils.check_config(config, pars, info_out, smp_trn, smp_tst)
 
     # initialization
     err = 0;
@@ -50,44 +53,44 @@ def main( conf_file='config.cfg' ):
     err_list = list()
     cls_list = list()
     it_list = list()
-    
+
     terr_list = list()
     tcls_list = list()
     titr_list = list()
     # the temporal weights
     malis_weights=[]
-    
+
     # interactive visualization
     plt.ion()
     plt.show()
-    
+
     start = time.time()
     for i in xrange(1, pars['Max_iter'] ):
         vol_ins, lbl_outs = smp_trn.get_random_sample()
-         
+
         # forward pass
         vol_ins = utils.make_continuous(vol_ins, dtype='float32')
         props = net.forward( vol_ins )
-                
+
         # cost function and accumulate errors
         props, cerr, grdts = pars['cost_fn']( props, lbl_outs )
         err = err + cerr
         cls = cls + cost_fn.get_cls(props, lbl_outs)
-        
+
         # run backward pass
         grdts = utils.make_continuous(grdts, dtype='float32')
         net.backward( grdts )
-    
+
         if pars['is_malis'] :
             malis_weights = cost_fn.malis_weight(props, lbl_outs)
             grdts = utils.loa_mul(grdts, malis_weights)
-        
+
         if i%pars['Num_iter_per_test']==0:
             # test the net
             terr_list, tcls_list = test.znn_test(net, pars, smp_tst,\
                                             vn, terr_list, tcls_list)
             titr_list.append(i)
-    
+
         if i%pars['Num_iter_per_show']==0:
             # anneal factor
             eta = eta * pars['anneal_factor']
@@ -95,11 +98,11 @@ def main( conf_file='config.cfg' ):
             # normalize
             err = err / vn / pars['Num_iter_per_show']
             cls = cls / vn / pars['Num_iter_per_show']
-    
+
             err_list.append( err )
             cls_list.append( cls )
             it_list.append( i )
-            
+
             if pars['is_visual']:
                 # show results To-do: run in a separate thread
                 front_end.inter_show(start, i, err, cls, it_list, err_list, cls_list, \
@@ -114,16 +117,16 @@ def main( conf_file='config.cfg' ):
             cls = 0
             # reset time
             start = time.time()
-            
+
         if i%pars['Num_iter_per_save']==0:
             # save network
             print "save network"
             netio.save_network(net, pars['train_save_net'], num_iters=i)
             utils.save_statistics( pars, it_list, err_list, cls_list,\
                                     titr_list, terr_list, tcls_list)
-        
-    
-        
+
+
+
 if __name__ == '__main__':
     import sys
     if len(sys.argv)>1:
--- python/utils.py
+++ python/utils.py
@@ -6,17 +6,59 @@ Jingpeng Wu <jingpeng.wu@gmail.com>, 2015
 
 import numpy as np
 
+def check_config(config, pars, info_out, smp_trn, smp_tst):
+    """
+    check all the configuration and parameters
+
+    Parameters
+    ----------
+    config : python parser reading of config file.
+    pars : the parameters.
+    info_out : size information of outputs.
+    smp_trn : training sample.
+    smp_tst : test sample.
+    """
+    assert(len(info_out)==1)
+    name, outsz = info_out.popitem()
+    cf = pars['cost_fn_str']
+    # check the output type
+    if 'boundary' in pars['out_dtype']:
+        assert(outsz[0]==2)
+        assert('softmax' in cf or 'square' in cf)
+        for sample in smp_trn.samples:
+            im = sample.outputs[name]
+            for pp_type in im.pp_types:
+                assert('binary' in pp_type or 'one' in pp_type)
+        for sample in smp_tst.samples:
+            im = sample.outputs[name]
+            for pp_type in im.pp_types:
+                assert('binary' in pp_type or 'one' in pp_type)
+    elif 'aff' in pars['out_dtype']:
+        print "network output size: ", outsz
+        assert(outsz[0]==3)
+        assert('binomial' in cf)
+        for sample in smp_trn.samples:
+            im = sample.outputs[name]
+            for pp_type in im.pp_types:
+                assert('aff' in pp_type)
+        for sample in smp_tst.samples:
+            im = sample.outputs[name]
+            for pp_type in im.pp_types:
+                assert('aff' in pp_type)
+    else:
+        raise NameError('invalid out_type!')
+
 def make_continuous( d , dtype='float32'):
     """
     make the dictionary arrays continuous.
-    
+
     Parameters
     ----------
     d : dict, the input dictionary of 4D array.
-    
+
     Returns
     -------
-    d : dict, the inner array are continuous.    
+    d : dict, the inner array are continuous.
     """
     for name, arr in d.iteritems():
         d[name] = np.ascontiguousarray(arr, dtype=dtype)
@@ -44,10 +86,10 @@ def save_statistics( pars, it_list, err_list, cls_list,\
     fname = root + '_statistics_current.h5'
     if os.path.exists( fname ):
         os.remove( fname )
-    
+
     # save variables
     import h5py
-    f = h5py.File( fname )    
+    f = h5py.File( fname )
     f.create_dataset('train/it',  data=it_list)
     f.create_dataset('train/err', data=err_list)
     f.create_dataset('train/cls', data=cls_list)
@@ -55,8 +97,7 @@ def save_statistics( pars, it_list, err_list, cls_list,\
     f.create_dataset('test/err',  data=terr_list)
     f.create_dataset('test/cls',  data=tcls_list)
     f.close()
-    
+
     # move to new name
     fname2 = root + '_statistics.h5'
     os.rename(fname, fname2)
-    
