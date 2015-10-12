af09e954a3ba3bb3d11a7084feec186663b7da4c
jingpengwu <jingpeng.wu@gmail.com>
2015-10-12 18:47:40 -0400

    add compiled tifffile

---
+++ python/tifffile/__init__.py
@@ -0,0 +1,5 @@
+from .tifffile import imsave, imread, imshow, TiffFile, TiffWriter, TiffSequence
+
+__version__ = '0.7.0'
+__all__ = ('imsave', 'imread', 'imshow', 'TiffFile', 'TiffWriter',
+           'TiffSequence')
