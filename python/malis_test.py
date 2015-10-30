2b720e3afedba4de46d6544fa3d639bce04573c0
jingpengwu <jingpeng.wu@gmail.com>
2015-10-30 15:54:16 -0400

    fix some problems of malis

--- python/malis_test.py
+++ python/malis_test.py
@@ -4,25 +4,27 @@ import numpy as np
 import time
 import utils
 import matplotlib.pylab as plt
-
+import os
 #%% parameters
 z = 8
 # epsilone: a small number for log to avoind -infinity
 eps = 0.0000001
 
-# largest diameter
-Dm = 100
-Ds = 50
+# largest disk radius
+Dm = 500
+Ds = 500
 
 # whether using constrained malis
-is_constrained = True
-
-# fill the boundary hole
-is_fill_lbl_holes = True
+is_constrained = False
 
 # thicken boundary of label by morphological errosion
-erosion_size = 0
+erosion_size = 5
 
+# a small corner
+corner_size = 200
+
+# disk radius threshold
+DrTh = 10
 
 #%% read images
 bdm = emirt.emio.imread('../experiments/zfish/VD2D/out_sample91_output_0.tif')
@@ -32,9 +34,13 @@ lbl = emirt.volume_util.lbl_RGB2uint32(lbl)
 lbl = lbl[z,:,:]
 bdm = bdm[z,:,:]
 
+# only a corner for test
+if corner_size > 0:
+    lbl = lbl[:corner_size, :corner_size]
+    bdm = bdm[:corner_size, :corner_size]
+
 # fill label holes
-if is_fill_lbl_holes:
-    utils.fill_boundary_holes( lbl )
+utils.fill_boundary_holes( lbl )
    
 # increase boundary width
 if erosion_size>0:
@@ -44,6 +50,9 @@ if erosion_size>0:
     msk = binary_erosion(msk, structure=erosion_structure)
     lbl[msk==False] = 0
 
+# recompile and use cost_fn
+print "compile the cost function..."
+os.system('python compile.py cost_fn')
 import cost_fn
 start = time.time()
 if is_constrained:
@@ -62,20 +71,20 @@ print "plot the images"
 if is_constrained:
     mbdm = np.copy(bdm)
     mbdm[lbl>0] = 1
-    plt.subplot(231)
+    plt.subplot(241)
     plt.imshow(1-mbdm, cmap='gray', interpolation='nearest')
     plt.xlabel('merger constrained boundary map')
 
     sbdm = np.copy(bdm)
     sbdm[lbl==0] = 0
-    plt.subplot(234)
+    plt.subplot(245)
     plt.imshow(1-sbdm, cmap='gray', interpolation='nearest')
     plt.xlabel('splitter constrained boundary map')
 else:
-    plt.subplot(231)
+    plt.subplot(241)
     plt.imshow(1-bdm, cmap='gray', interpolation='nearest')
     plt.xlabel('boundary map')
-    plt.subplot(234)
+    plt.subplot(245)
     plt.imshow(lbl==0, cmap='gray', interpolation='nearest')
 #    emirt.show.random_color_show( lbl )
     plt.xlabel('manual labeling')
@@ -90,45 +99,84 @@ def combine2rgb(bdm, w):
     # make a combined colorful image
     cim = np.zeros(bdm.shape+(3,), dtype='uint8')
     # red channel
-    cim[:,:,0] = (rescale(1-bdm))*255
+    cim[:,:,0] = (rescale(bdm))*255
     # green channel
     cim[:,:,1] = rescale( w )*255
     return cim
+    
+def disk_plot(me, Dm, color='g'):
+    # plot disk to illustrate the weight strength
+    # rescale to 0-1
+    rme = rescale(me) * Dm
+    my,mx = np.nonzero(rme)
+    mr = rme[(my,mx)]
+    # sort the disk from small to large
+    locs = np.argsort( mr )
+    my = my[ locs ]
+    mx = mx[ locs ]
+    mr = mr[ locs ]
+    # eleminate the small disks
+    my = my[ mr > DrTh ]
+    mx = mx[ mr > DrTh ]
+    mr = mr[ mr > DrTh ]
+    plt.scatter(mx,my,mr, c=color, alpha=0.8, linewidths=0)
+
 
 # combine merging error with boundary map
-rgbm = combine2rgb(bdm, np.log(me+eps))
-plt.subplot(232)
+rgbm = combine2rgb(1-bdm, np.log(me+eps))
+plt.subplot(242)
 plt.imshow( rgbm, interpolation='nearest' )
 plt.xlabel('combine boundary map(red) and ln(merge weight)(green)')
 
 # combine merging error with boundary map
-rgbs = combine2rgb(bdm, np.log(se+eps))
-plt.subplot(235)
+rgbs = combine2rgb(1-bdm, np.log(se+eps))
+plt.subplot(246)
 plt.imshow( rgbs, interpolation='nearest' )
 plt.xlabel('combine boundary map(red) and ln(split weight)(green)')
 
 # combine merging error with boundary map
-rgbm = combine2rgb(bdm, me)
-plt.subplot(233)
+rgbm = combine2rgb(1-bdm, me)
+plt.subplot(243)
 plt.imshow( rgbm, interpolation='nearest' )
 plt.xlabel('combine boundary map(red) and merge weight(green disk)')
-# plot disk to illustrate the weight strength
-# rescale to 0-1
-rme = rescale(me) * Dm
-y,x = np.nonzero(rme)
-r = rme[(y,x)]
-plt.scatter(x,y,r, c='g', alpha=0.8)
+disk_plot(me, Dm)
 
-# combine merging error with boundary map
-rgbs = combine2rgb(bdm, se)
-plt.subplot(236)
+plt.subplot(247)
 plt.imshow( rgbs, interpolation='nearest' )
 plt.xlabel('combine boundary map(red) and split weight(green disk)')
-# plot disk to illustrate the weight strength
-# rescale to 0-1
-rse = rescale(se) * Ds
-y,x = np.nonzero(rse)
-r = rse[(y,x)]
-plt.scatter(x,y,r, c='g', alpha=0.8)
+disk_plot(se, Ds)
+
+# gradient
+# square loss gradient
+grdt = 2 * (bdm-lbl)
+# merger and splitter gradient
+mg = grdt * me
+sg = grdt * se
+
+def gradient2rgb(g):
+    # positive part
+    pg = np.zeros(g.shape, g.dtype)
+    pg[g>0] = g[g>0]
+    # negative part
+    ng = np.zeros(g.shape, g.dtype)
+    ng[g<0] = g[g<0]
+    # combine to RGB image
+    cim = combine2rgb(pg, ng)
+    return cim, pg, ng
+    
+
+plt.subplot(244)
+cim,mpg,mng = gradient2rgb(mg)
+disk_plot( mpg, Dm/10, color='r')
+disk_plot( mng, Dm/10, color='g')
+plt.xlabel('merger gradient (square loss)')
+
+
+plt.subplot(248)
+cim,spg,sng = gradient2rgb(sg)
+disk_plot( spg, Ds/10, color='r' )
+disk_plot( sng, Ds/10, color='g' )
+#plt.imshow(gradient2rgb(sg), interpolation='nearest')
+plt.xlabel('splitter gradient (square loss)')
 
 plt.show()
