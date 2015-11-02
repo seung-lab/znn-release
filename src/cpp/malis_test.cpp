05b6c298b7f4c4b984f43bd4b8e159b4153156e1
jingpengwu <jingpeng.wu@gmail.com>
2015-11-02 14:28:33 -0500

    update of c++ code

--- src/cpp/malis_test.cpp
+++ src/cpp/malis_test.cpp
@@ -28,14 +28,14 @@ public:
     inline void _mark_line(cube_p<T> mat, int row, T val, int col=0)
     {
     	if(col==0)
-    	{	// erase the whole line
-    		for (std::size_t i; i<size_[1]; i++)
-    			(*mat)[row][i][0] = val;
+    	{   // erase the whole line
+            for (std::size_t i; i<size_[1]; i++)
+                (*mat)[row][i][0] = val;
     	}
     	else
     	{
-    		// change one value
-    		(*mat)[row][col][0] = val;
+            // change one value
+            (*mat)[row][col][0] = val;
     	}
     	return;
     }
@@ -43,14 +43,28 @@ public:
 	template<typename T>
 	inline CMalis_test()
 	{
-		std::size_t s=10;
-		// create the image
-		image = get_cube<T>(s,s,1);
-		//
+            std::size_t s=10;
+            // create the image
+            image = get_cube<T>(s,s,1);
+            for (std::size_t x=0; x<s; x++)
+                for (std::size_t y=0; y<s; y++)
+                    (*image)[x][y][0] = 1;
+            _make_line(image, 3, 0.5);
+            _make_line(image, 3, 0.8, 7);
+            _make_line(image, 3, 0.2, 3);
+            _make_line(image, 6, 0.5);
+            _make_line(image, 6, 0.2, 3);
+            _make_line(image, 6, 0.8, 7);
 
-		label = get_cube<T>(s,s,1);
+            // create label
+            label = get_cube<T>(s,s,1);
+            for (std::size_t x=0; x<6; x++)
+                _make_line(label, x, 1);
+            _make_line(label, 6, 0);
+            for (std::size_t x = 7; x<s; x++)
+                _make_line(label, x, 2);
 
-		return;
+            return;
 	}
 
 };
