113d2bb3fd3a20ef0bc2a6b506ed5214952908a0
Aleksandar Zlateski <zlateski@mit.edu>
2015-11-11 16:17:09 -0500

    convolver impl started

---
+++ src/include/convolution/convolver.hpp
@@ -0,0 +1,33 @@
+//
+// Copyright (C) 2012-2015  Aleksandar Zlateski <zlateski@mit.edu>
+// ---------------------------------------------------------------
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+#pragma once
+
+#include "../types.hpp"
+
+namespace znn { namespace v4 {
+
+template< typename T >
+class convolver
+{
+public:
+    virtual cube_p<T> forward( ccube_p<T> const &, ccube_p<T> const & ) = 0;
+    virtual cube_p<T> flipped( ccube_p<T> const &, ccube_p<T> const & ) = 0;
+    virtual cube_p<T> inverse( ccube_p<T> const &, ccube_p<T> const & ) = 0;
+};
+
+}} // namespace znn::v4
