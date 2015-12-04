d990da46f60a629f28769354ea5517507ad69458
nicholasturner1 <nturner.stanford@gmail.com>
2015-12-03 21:26:12 -0500

    aligning test normalization with train.py, spelling

--- python/front_end/zsample.py
+++ python/front_end/zsample.py
@@ -447,16 +447,16 @@ class CBoundarySample(CSample):
     def get_random_sample(self):
         subimgs, sublbls, submsks = super(CBoundarySample, self).get_random_sample()
 
-        # boudary map rebalance
+        # boundary map rebalance
         subwmsks = dict()
         for key, sublbl in sublbls.iteritems():
             subwmsks[key] = self._rebalance_bdr(  sublbl, self.wps[key], self.wzs[key] )
 
         for key,sublbl in sublbls.iteritems():
             assert sublbl.ndim==3 or (sublbl.ndim==4 and sublbl.shape[0]==1)
-            # binarize the true lable
+            # binarize the true label
             sublbls[key] = self._binary_class( sublbl )
-            # duplicate the maskes
+            # duplicate the masks
             submsks[key]  = np.tile(submsks[key], (2,1,1,1))
             subwmsks[key] = np.tile(subwmsks[key], (2,1,1,1))
 
