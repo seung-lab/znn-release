b728c54ea0c5804be4ee6a36bf130184af2413d5
jingpengwu <jingpeng.wu@gmail.com>
2016-01-14 11:36:07 -0500

    update compare.jl

--- julia/compare.jl
+++ julia/compare.jl
@@ -1,10 +1,34 @@
+using ArgParse
 using HDF5
 using EMIRT
 
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--flbl"
+        help = "label file name"
+        required = true
+
+        "--faffs"
+        help = "affinity file names, could be multiple files to compare"
+        required = true
+
+        "--step"
+        help = "error curve step"
+        arg_type = Float64
+        default = 0.1
+    end
+    return parse_args(s)
+end
+
+#function evaluate_aff_file(flbl, faffs)
+
+
 # parameters
 #faff = "/znn/experiments/malis21/N4_A_1000/out_sample91_output.h5"
-#faff = "/znn/experiments/zfish/N4_A/out_sample91_output.h5"
-faff = "/znn/experiments/malis2/N4_A_100/out_sample91_output.h5"
+faff = "/znn/experiments/zfish/N4_A/out_sample91_output.h5"
+#faff = "/znn/experiments/malis2/N4_A_100/out_sample91_output.h5"
 flbl = "/znn/dataset/zfish/Merlin_lbl2.h5"
 
 # read affinity data
@@ -19,13 +43,18 @@ affs = h5read(faff, "/main");
 lbl = h5read(flbl, "/main")
 
 # rand error curve
-thds, res = affs_rand_error_curve(affs, lbl, 0.05, 2)
+thds, rfs, res = affs_error_curve(affs, lbl, 2, 0.05)
 
 # save curve
-h5write(faff, "/evaluate/thds", thds)
-h5write(faff, "/evaluate/res",  res )
+fcurve = "/znn/experiments/curves.h5"
+h5write(fcurve, "/zfish/thds", thds)
+h5write(fcurve, "/zfish/rfs",  rfs )
+h5write(fcurve, "/zfish/res",  res )
 
 # plot
 using PyPlot
+subplot(121)
 plot(thds, res)
+subplot(122)
+plot(thds, rfs)
 show()
