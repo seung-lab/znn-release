6563eeca78ba8140a727fff59d65282063815aba
nicholasturner1 <nturner.stanford@gmail.com>
2016-02-01 23:38:43 -0500

    intermediate commit to fix boundary mirroring

--- python/train.py
+++ python/train.py
@@ -74,9 +74,9 @@ def main( args ):
     # initialize samples
     outsz = pars['train_outsz']
     print "\n\ncreate train samples..."
-    smp_trn = zsample.CThreadedSamples(config, pars, pars['train_range'], net, outsz, logfile)
+    smp_trn = zsample_thr.CThreadedSamples_S3(config, pars, pars['train_range'], net, outsz, logfile)
     print "\n\ncreate test samples..."
-    smp_tst = zsample.CSamples(config, pars, pars['test_range'],  net, outsz, logfile)
+    smp_tst = zsample_thr.CThreadedSamples_S3(config, pars, pars['test_range'],  net, outsz, logfile)
 
     if pars['is_check']:
         import zcheck
