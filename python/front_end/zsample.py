43023b57dead392f93f1979b59327e4f5fb9c17a
torms3 <kisuklee@mit.edu>
2016-03-18 03:21:08 -0400

    balance the probability of choosing one sample

--- python/front_end/zsample.py
+++ python/front_end/zsample.py
@@ -515,6 +515,11 @@ class CSamples(object):
         #Parameter object
         self.pars = pars
 
+        # probability of choosing this sample
+        self.smp_prbs = list()
+        # total number of candidate locations
+        Nloc = 0.0
+
         self.samples = list()
         for sid in ids:
             if 'bound' in pars['out_type']:
@@ -524,8 +529,18 @@ class CSamples(object):
             else:
                 raise NameError('invalid output type')
             self.samples.append( sample )
+            self.smp_prbs.append(len(sample.locs))
+            Nloc += len(sample.locs)
+
+        # normalize the number of locations to probability
+        for i,p in enumerate(self.smp_prbs):
+            self.smp_prbs[i] = self.smp_prbs[i] / Nloc
 
     def get_random_sample(self):
         '''Fetches a random sample from a random CSample object'''
-        i = np.random.randint( len(self.samples) )
+        # get the sequence of multinomial sequence.
+        # only one element is 1, others are 0
+        sq = np.random.multinomial(1, self.smp_prbs, size=1)
+        # get the index of non-zero element
+        i = np.nonzero(sq)[1][0]
         return self.samples[i].get_random_sample()
