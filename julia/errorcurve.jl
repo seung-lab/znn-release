bff98eef320c1c0a204388c41d8b84d502f2cb55
jingpengwu <jingpeng.wu@gmail.com>
2016-03-28 17:18:35 -0400

    simplify the command line parsing using EMIRT parser

--- julia/errorcurve.jl
+++ julia/errorcurve.jl
@@ -3,105 +3,44 @@ using HDF5
 using EMIRT
 using PyPlot
 
-function parse_commandline()
-    s = ArgParseSettings()
-
-    @add_arg_table s begin
-        "--tag"
-        help = "curve type name"
-        required = true
-        arg_type = ASCIIString
-
-        "--flbl"
-        help = "label file name"
-        required = true
-        arg_type = ASCIIString
-
-        "--faffs"
-        help = "affinity file names, could be multiple files to compare"
-        required = true
-        arg_type = ASCIIString
-
-        "--step"
-        help = "error curve step"
-        arg_type = Float64
-        default = 0.1
-
-        "--fcurve"
-        help = "file to save the error curve"
-        arg_type = ASCIIString
-        default = "/tmp/error_curve.h5"
-
-        "--seg_method"
-        help = "segmentation method: connected_component; watershed"
-        arg_type = ASCIIString
-        default = "connected_component"
-
-        "--dim"
-        help = "segmentation and evaluation dimention. 2/3"
-        arg_type = Int64
-        default = 2
-
-        "--is_patch"
-        help = "use patch based segmentation error"
-        arg_type = Bool
-        default = false
-
-        "--is_plot"
-        help = "whether plot the curve or not"
-        arg_type = Bool
-        default = false
-    end
-    return parse_args(s)
-end
-
 function main()
-    # read command line parameters
-    faffs = ""
-    flbl = ""
-    tag = ""
-    step = 0.1
-    seg_method = ""
-    dim = 2
-    fcurve = ""
-    is_plot = false
-    is_patch = false
-    for pa in parse_commandline()
-        if pa[1] == "tag"
-            tag = pa[2]
-        elseif pa[1] == "flbl"
-            flbl = pa[2]
-        elseif pa[1] == "faffs"
-            faffs = pa[2]
-        elseif pa[1] == "step"
-            step = pa[2]
-        elseif pa[1] == "seg_method"
-            seg_method = pa[2]
-        elseif pa[1] == "dim"
-            dim = pa[2]
-        elseif pa[1] == "fcurve"
-            fcurve = pa[2]
-        elseif pa[1] == "is_plot"
-            is_plot = pa[2]
-        elseif pa[1] == "is_patch"
-            is_patch = pa[2]
-        end
-    end
+    pd = Dict('t'=>"tag",
+              'a'=>"/tmp/affs.h5",
+              'l'=>"/tmp/label.h5",
+              'd'=>3,
+              's'=>0.1,
+              'g'=>"watershed",
+              'p'=>false,
+              'o'=>"/tmp/error_curve.h5"
+              )
+    argparser!(pd)
 
-    @assert dim==2 || dim==3
+    @assert pd['d']==2 || pd['d']==3
+
+    # save the parameter in output error curve file
+    fcurve = pd['o']
+    if isfile(fcurve)
+        rm(fcurve)
+    end
+    # tag = pd['t']
+    # for (k,v) in pd
+    #     k = join(k)
+    #     h5write(fcurve, "/$tag/$k", v)
+    # end
+
     # read data
     # read affinity data
-    affs = EMIRT.imread(faffs);
+    affs = EMIRT.imread( pd['a'] );
     # exchange X and Z channel
     # exchangeaffxz!(affs)
 
     # read label ground truth
-    lbl = EMIRT.imread(flbl)
+    lbl = EMIRT.imread( pd['l'] )
     lbl = Array{UInt32,3}(lbl)
 
     # rand error and rand f score curve, both are foreground restricted
     print("compute error curves of affinity map ......")
-    thds, segs, rf, rfm, rfs, re, rem, res = affs_error_curve(affs, lbl, dim, step, seg_method, is_patch)
+    thds, segs, rf, rfm, rfs, re, rem, res = affs_error_curve(affs, lbl, pd['d'], pd['s'], pd['g'], pd['p'])
     print("done :)")
 
     # save the curve
