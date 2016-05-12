fixed the learning problem by return props from cost computation

python/cost_fn.py
@@ -182,13 +182,10 @@ def multinomial_cross_entropy(props, lbls, mask=None):
 
     for name, prop in props.iteritems():
         lbl = lbls[name]
-
         grdts[name] = prop - lbl
-
         entropy[name] = -lbl * np.log(prop)
 
     #Applying mask if it exists
-    grdts = utils.mask_dict_vol(grdts, mask)
     entropy = utils.mask_dict_vol(entropy, mask)
 
     for name, vol in entropy.iteritems():
@@ -436,4 +433,4 @@ def get_grdt(pars, history, props, lbl_outs, msks, wmsks, vn):
         history['me'] += dme.values()[0]
 
     grdts = utils.make_continuous(grdts)
-    return history, grdts
+    return props, grdts, history

python/front_end/zsample.py
@@ -452,7 +452,6 @@ class CBoundarySample(CSample):
 
         return weight
 
-
     def get_random_sample(self):
         subimgs, sublbls, submsks = super(CBoundarySample, self).get_random_sample()
 
@@ -460,14 +459,14 @@ class CBoundarySample(CSample):
         subwmsks = dict()
         for key, sublbl in sublbls.iteritems():
             submsk = submsks[key]
-            subwmsks[key] = self._rebalance_bdr( sublbl, submsk, self.wps[key], self.wzs[key] )
+            subwmsk = self._rebalance_bdr( sublbl, submsk, self.wps[key], self.wzs[key] )
+            subwmsks[key] = subwmsk
 
-        for key,sublbl in sublbls.iteritems():
             assert sublbl.ndim==3 or (sublbl.ndim==4 and sublbl.shape[0]==1)
             # binarize the true lable
             sublbls[key] = self._binary_class( sublbl )
-            # duplicate the maskes
-            submsks[key]  = np.tile(submsks[key], (2,1,1,1))
+            # duplicate the masks
+            submsks[key]  = np.tile(submsk, (2,1,1,1))
             subwmsks[key] = np.tile(subwmsks[key], (2,1,1,1))
 
         return subimgs, sublbls, submsks, subwmsks