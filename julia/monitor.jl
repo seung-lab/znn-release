11340bd8cdae484604a84dbd3f6000c742317a45
jingpengwu <jingpeng.wu@gmail.com>
2016-05-16 18:30:32 +0000

    functional version

--- julia/monitor.jl
+++ julia/monitor.jl
@@ -2,35 +2,47 @@ using Gadfly
 using Distributions
 using HDF5
 
-function get_learning_curve(fname)
-    # if "s3://" in fname
-    #     # local file name
-    #     lcfname = "./net_current.h5"
-    #     # download from  AWS S3
-    #     run(`aws s3 cp $(fname) $(lcfname)`)
-    #     # rename fname to local file name
-    #     fname = lcfname
-    # end
-    it  = h5read(fname, "/processing/znn/train/statistics/train/it")
-    err = h5read(fname, "/processing/znn/train/statistics/train/err")
-    cls = h5read(fname, "/processing/znn/train/statistics/train/cls")
-    return it, err, cls
+function get_learning_curve(fname::AbstractString)
+    if "s3://" in fname
+        # local file name
+        lcfname = "/tmp/net_current.h5"
+        # download from  AWS S3
+        run(`aws s3 cp $(fname) $(lcfname)`)
+        # rename fname to local file name
+        fname = lcfname
+    end
+    curve = Dict{ASCIIString, Dict{ASCIIString,Vector}}()
+    if isfile(fname)
+        curve["train"]["it"]  = h5read(fname, "/processing/znn/train/statistics/train/it")
+        curve["train"]["err"] = h5read(fname, "/processing/znn/train/statistics/train/err")
+        curve["train"]["cls"] = h5read(fname, "/processing/znn/train/statistics/train/cls")
+        curve["test"]["it"]   = h5read(fname, "/processing/znn/train/statistics/test/it")
+        curve["test"]["err"]  = h5read(fname, "/processing/znn/train/statistics/test/err")
+        curve["test"]["cls"]  = h5read(fname, "/processing/znn/train/statistics/test/cls")
+    end
+    return curve
 end
 
-function plotcurve(fname)
-    it, err, cls = get_learning_curve(fname)
-    cvplot = vbox(md"## Learning Curve of Cost",
-                  drawing(8Gadfly.inch, 4Gadfly.inch,
-                          plot(x=it/1000, y=err, Geom.line,
-                               Guide.xlabel("Iteration (K)"), Guide.ylabel("Cost"),
-                               Guide.title("Learning Curve of Cost"))),
-                  md"## Learning Curve of Pixel Error",
-                  drawing(8Gadfly.inch, 4Gadfly.inch,
-                          plot(x=it/1000, y=cls, Geom.line,
-                               Guide.xlabel("Iteration (K)"), Guide.ylabel("Pixel Error"),
-                               Guide.title("Learning Curve of Pixel Error")))
-                  ) |> Escher.pad(2em)
-    return cvplot
+function plotcurve(curve::Dict)
+    return vbox(
+    md"## Learning Curve of Cost",
+    drawing(8Gadfly.inch, 4Gadfly.inch,
+            plot(layer(x=curve["train"]["it"]/1000, y=curve["train"]["err"], Geom.line),
+                 layer(x=curve["test"]["it"] /1000, y=curve["test"]["err"],  Geom.line),
+                 Guide.xlabel("Iteration (K)"), Guide.ylabel("Cost"),
+                 Guide.title("Learning Curve of Cost"))),
+    md"## Learning Curve of Pixel Error",
+    drawing(8Gadfly.inch, 4Gadfly.inch,
+            plot(layer(x=curve["train"]["it"]/1000, y=curve["train"]["cls"], Geom.line),
+                 layer(x=curve["test"]["it"] /1000, y=curve["test"]["cls"],  Geom.line),
+                 Guide.xlabel("Iteration (K)"), Guide.ylabel("Pixel Error"),
+                 Guide.title("Learning Curve of Pixel Error"))),
+    ) |> Escher.pad(2em)
+end
+
+function plotcurve(fname::AbstractString)
+    curve = get_learning_curve(fname)
+    return plotcurve(curve)
 end
 
 main(window) = begin
@@ -49,8 +61,7 @@ main(window) = begin
         vbox(
              intent(s, form) >>> inp,
              vskip(2em),
-             plotcurve("./net_current.h5"),
-             get(dict, :fname, ""),
+             plotcurve(get(dict, :fname, "")),
              string( keys(dict) ),
              string( dict )
              ) |> Escher.pad(2em)
