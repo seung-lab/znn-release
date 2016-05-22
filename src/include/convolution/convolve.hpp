eec7e248af2a33697fec87ccc750f24dc3327eb3
torms3 <kisuklee@mit.edu>
2016-05-22 00:15:45 -0400

    [qube] qube version convolve

--- src/include/convolution/convolve.hpp
+++ src/include/convolution/convolve.hpp
@@ -26,6 +26,7 @@
 
 namespace znn { namespace v4 {
 
+// cube
 template< typename T >
 inline void convolve_add( cube<T> const & a,
                           cube<T> const & b,
@@ -62,6 +63,48 @@ inline void convolve_add( cube<T> const & a,
                             r[x][y][z] += a[dx][dy][dz] * b[wx][wy][wz];
 }
 
+// qube
+template< typename T >
+inline void convolve_add( qube<T> const & a,
+                          cube<T> const & b,
+                          qube<T> & r) noexcept
+{
+    if ( b.num_elements() == 1 )
+    {
+        convolve_constant_add(a,b.data()[0],r);
+        return;
+    }
+
+    size_t am = a.shape()[0];
+    size_t ax = a.shape()[1];
+    size_t ay = a.shape()[2];
+    size_t az = a.shape()[3];
+
+    size_t bx = b.shape()[0];
+    size_t by = b.shape()[1];
+    size_t bz = b.shape()[2];
+
+    size_t rm = am;
+    size_t rx = ax - bx + 1;
+    size_t ry = ay - by + 1;
+    size_t rz = az - bz + 1;
+
+    ZI_ASSERT(r.shape()[0]==rm);
+    ZI_ASSERT(r.shape()[1]==rx);
+    ZI_ASSERT(r.shape()[2]==ry);
+    ZI_ASSERT(r.shape()[3]==rz);
+
+    for ( size_t m = 0; m < rm; ++m )
+        for ( size_t x = 0; x < rx; ++x )
+            for ( size_t y = 0; y < ry; ++y )
+                for ( size_t z = 0; z < rz; ++z )
+                    for ( size_t dx = x, wx = bx - 1; dx < bx + x; ++dx, --wx )
+                        for ( size_t dy = y, wy = by - 1; dy < by + y; ++dy, --wy )
+                            for ( size_t dz = z, wz = bz - 1; dz < bz + z; ++dz, --wz )
+                                r[m][x][y][z] += a[m][dx][dy][dz] * b[wx][wy][wz];
+}
+
+// cube
 template< typename T >
 inline void convolve( cube<T> const & a,
                       cube<T> const & b,
@@ -77,6 +120,23 @@ inline void convolve( cube<T> const & a,
     convolve_add(a,b,r);
 }
 
+// qube
+template< typename T >
+inline void convolve( qube<T> const & a,
+                      cube<T> const & b,
+                      qube<T> & r) noexcept
+{
+    if ( b.num_elements() == 1 )
+    {
+        convolve_constant(a,b.data()[0],r);
+        return;
+    }
+
+    fill(r,0);
+    convolve_add(a,b,r);
+}
+
+// cube
 template< typename T >
 inline cube_p<T> convolve( cube<T> const & a,
                            cube<T> const & b)
@@ -86,6 +146,31 @@ inline cube_p<T> convolve( cube<T> const & a,
     return r;
 }
 
+// qube
+template< typename T >
+inline qube_p<T> convolve( qube<T> const & a,
+                           cube<T> const & b)
+{
+    size_t am = a.shape()[0];
+    size_t ax = a.shape()[1];
+    size_t ay = a.shape()[2];
+    size_t az = a.shape()[3];
+
+    size_t bx = b.shape()[0];
+    size_t by = b.shape()[1];
+    size_t bz = b.shape()[2];
+
+    size_t rm = am;
+    size_t rx = ax - bx + 1;
+    size_t ry = ay - by + 1;
+    size_t rz = az - bz + 1;
+
+    cube_p<T> r = get_qube<T>(rm,rx,ry,rz);
+    convolve(a,b,*r);
+    return r;
+}
+
+// cube
 template< typename T >
 inline cube_p<T> convolve( ccube_p<T> const & a,
                            ccube_p<T> const & b)
@@ -93,7 +178,16 @@ inline cube_p<T> convolve( ccube_p<T> const & a,
     return convolve(*a, *b);
 }
 
+// qube
+template< typename T >
+inline qube_p<T> convolve( qcube_p<T> const & a,
+                           ccube_p<T> const & b)
+{
+    return convolve(*a, *b);
+}
 
+
+// cube
 template< typename T >
 inline void convolve_flipped_add( cube<T> const & a,
                                   cube<T> const & b,
@@ -133,7 +227,53 @@ inline void convolve_flipped_add( cube<T> const & a,
                                 b[bx-1-dx][by-1-dy][bz-1-dz];
 }
 
+// qube
+template< typename T >
+inline void convolve_flipped_add( qube<T> const & a,
+                                  cube<T> const & b,
+                                  qube<T> & r) noexcept
+{
+    size_t am = a.shape()[0];
+    size_t ax = a.shape()[1];
+    size_t ay = a.shape()[2];
+    size_t az = a.shape()[3];
 
+    // TODO(lee): convolve_constant_filpped for qube
+    // if ( vec3i(ax,ay,az) == size(b) )
+    // {
+    //     ZI_ASSERT(r.num_elements()==am);
+    //     for ( size_t m = 0; m < rm; ++m )
+    //         r.data()[0] += convolve_constant_flipped(a,b);
+    //     return;
+    // }
+
+    size_t bx = b.shape()[0];
+    size_t by = b.shape()[1];
+    size_t bz = b.shape()[2];
+
+    size_t rm = am;
+    size_t rx = ax - bx + 1;
+    size_t ry = ay - by + 1;
+    size_t rz = az - bz + 1;
+
+    ZI_ASSERT(r.shape()[0]==rm);
+    ZI_ASSERT(r.shape()[1]==rx);
+    ZI_ASSERT(r.shape()[2]==ry);
+    ZI_ASSERT(r.shape()[3]==rz);
+
+    for ( size_t m = 0; m < rm; ++m )
+        for ( size_t x = 0; x < rx; ++x )
+            for ( size_t y = 0; y < ry; ++y )
+                for ( size_t z = 0; z < rz; ++z )
+                    for ( size_t dx = 0; dx < bx; ++dx )
+                        for ( size_t dy = 0; dy < by; ++dy )
+                            for ( size_t dz = 0; dz < bz; ++dz )
+                                r[m][x][y][z] +=
+                                    a[m][ax-1-x-dx][ay-1-y-dy][az-1-z-dz] *
+                                    b[bx-1-dx][by-1-dy][bz-1-dz];
+}
+
+// cube
 template< typename T >
 inline void convolve_flipped( cube<T> const & a,
                               cube<T> const & b,
@@ -150,7 +290,25 @@ inline void convolve_flipped( cube<T> const & a,
     convolve_flipped_add(a,b,r);
 }
 
+// qube
+template< typename T >
+inline void convolve_flipped( qube<T> const & a,
+                              cube<T> const & b,
+                              qube<T> & r) noexcept
+{
+    // TODO(lee): convolve_constant_flipped for qube
+    // if ( size(a) == size(b) )
+    // {
+    //     ZI_ASSERT(r.num_elements()==1);
+    //     r.data()[0] = convolve_constant_flipped(a,b);
+    //     return;
+    // }
 
+    fill(r,0);
+    convolve_flipped_add(a,b,r);
+}
+
+// cube
 template< typename T >
 inline cube_p<T> convolve_flipped( cube<T> const & a,
                                    cube<T> const & b)
@@ -160,6 +318,31 @@ inline cube_p<T> convolve_flipped( cube<T> const & a,
     return r;
 }
 
+// qube
+template< typename T >
+inline qube_p<T> convolve_flipped( qube<T> const & a,
+                                   cube<T> const & b)
+{
+    size_t am = a.shape()[0];
+    size_t ax = a.shape()[1];
+    size_t ay = a.shape()[2];
+    size_t az = a.shape()[3];
+
+    size_t bx = b.shape()[0];
+    size_t by = b.shape()[1];
+    size_t bz = b.shape()[2];
+
+    size_t rm = am;
+    size_t rx = ax - bx + 1;
+    size_t ry = ay - by + 1;
+    size_t rz = az - bz + 1;
+
+    qube_p<T> r = get_qube<T>(rm,rx,ry,rz);
+    convolve_flipped(a,b,*r);
+    return r;
+}
+
+// cube
 template< typename T >
 inline cube_p<T> convolve_flipped( ccube_p<T> const & a,
                                    ccube_p<T> const & b)
@@ -167,7 +350,16 @@ inline cube_p<T> convolve_flipped( ccube_p<T> const & a,
     return convolve_flipped(*a, *b);
 }
 
+// qube
+template< typename T >
+inline qube_p<T> convolve_flipped( cqube_p<T> const & a,
+                                   ccube_p<T> const & b)
+{
+    return convolve_flipped(*a, *b);
+}
 
+
+// cube
 template< typename T >
 inline void convolve_inverse_add( cube<T> const & a,
                                   cube<T> const & b,
@@ -208,6 +400,52 @@ inline void convolve_inverse_add( cube<T> const & a,
             }
 }
 
+// qube
+template< typename T >
+inline void convolve_inverse_add( qube<T> const & a,
+                                  cube<T> const & b,
+                                  qube<T> & r) noexcept
+{
+    // TODO(lee): convolve_constant_inverse_add for qube
+    if ( size(b) == vec3i::one )
+    {
+        convolve_constant_inverse_add(a,b.data()[0],r);
+        return;
+    }
+
+    size_t am = a.shape()[0];
+    size_t ax = a.shape()[1];
+    size_t ay = a.shape()[2];
+    size_t az = a.shape()[3];
+
+    size_t bx = b.shape()[0];
+    size_t by = b.shape()[1];
+    size_t bz = b.shape()[2];
+
+    ZI_ASSERT(r.shape()[0]==am);
+    ZI_ASSERT(r.shape()[1]==ax + bx - 1);
+    ZI_ASSERT(r.shape()[2]==ay + by - 1);
+    ZI_ASSERT(r.shape()[3]==az + bz - 1);
+
+    for ( size_t m = 0; m < rm; ++m )
+        for ( size_t dx = 0; dx < bx; ++dx )
+            for ( size_t dy = 0; dy < by; ++dy )
+                for ( size_t dz = 0; dz < bz; ++dz )
+                {
+                    size_t fx = bx - 1 - dx;
+                    size_t fy = by - 1 - dy;
+                    size_t fz = bz - 1 - dz;
+
+                    for ( size_t x = 0; x < ax; ++x )
+                        for ( size_t y = 0; y < ay; ++y )
+                            for ( size_t z = 0; z < az; ++z )
+                            {
+                                r[m][x+fx][y+fy][z+fz] += a[m][x][y][z] * b[dx][dy][dz];
+                            }
+                }
+}
+
+// cube
 template< typename T >
 inline void convolve_inverse( cube<T> const & a,
                               cube<T> const & b,
@@ -223,7 +461,24 @@ inline void convolve_inverse( cube<T> const & a,
     convolve_inverse_add(a,b,r);
 }
 
+// qube
+template< typename T >
+inline void convolve_inverse( qube<T> const & a,
+                              cube<T> const & b,
+                              qube<T> & r) noexcept
+{
+    // TODO(lee): convolve_constant_inverse for qube
+    if ( size(b) == vec3i::one )
+    {
+        convolve_constant_inverse(a,b.data()[0],r);
+        return;
+    }
 
+    fill(r,0);
+    convolve_inverse_add(a,b,r);
+}
+
+// cube
 template< typename T >
 inline cube_p<T> convolve_inverse( cube<T> const & a,
                                    cube<T> const & b)
@@ -233,8 +488,41 @@ inline cube_p<T> convolve_inverse( cube<T> const & a,
     return r;
 }
 
+// qube
+template< typename T >
+inline qube_p<T> convolve_inverse( qube<T> const & a,
+                                   cube<T> const & b)
+{
+    size_t am = a.shape()[0];
+    size_t ax = a.shape()[1];
+    size_t ay = a.shape()[2];
+    size_t az = a.shape()[3];
+
+    size_t bx = b.shape()[0];
+    size_t by = b.shape()[1];
+    size_t bz = b.shape()[2];
+
+    size_t rm = am;
+    size_t rx = ax - bx + 1;
+    size_t ry = ay - by + 1;
+    size_t rz = az - bz + 1;
+
+    qube_p<T> r = get_qube<T>(rm,rx,ry,rz);
+    convolve_inverse(a,b,*r);
+    return r;
+}
+
+// cube
 template< typename T >
 inline cube_p<T> convolve_inverse( ccube_p<T> const & a,
+                                   ccube_p<T> const & b)
+{
+    return convolve_inverse(*a, *b);
+}
+
+// qube
+template< typename T >
+inline qube_p<T> convolve_inverse( cqube_p<T> const & a,
                                    ccube_p<T> const & b)
 {
     return convolve_inverse(*a, *b);
